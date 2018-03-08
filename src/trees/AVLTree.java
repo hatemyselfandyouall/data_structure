c653b825c5e397ad5dc5faf4801465a4e859b8fc
高正德 <gaozhengde@zhuojianchina.com>
2018-03-08 20:31:43 +0800

    test

--- src/trees/AVLTree.java
+++ src/trees/AVLTree.java
@@ -1,9 +1,13 @@
 package trees;
 
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.LinkedList;
+
 /**
  * Created by YS-GZD-1495 on 2018/3/6.
  */
-public class AVLTree extends TwoForkLookupTree {
+public class AVLTree   {
     private Node root;
 
     class Node{
@@ -11,11 +15,13 @@ public class AVLTree extends TwoForkLookupTree {
         Node leftChild;
         Node rightChild;
         int height;
-        Node(int val,Node leftChild,Node rightChild,int height){
+        int right;
+        Node(int val,Node leftChild,Node rightChild,int height,int right){
             this.val=val;
             this.leftChild=leftChild;
             this.rightChild=rightChild;
             this.height=height;
+            this.right=right;
         }
         @Override
         public String toString() {
@@ -24,23 +30,36 @@ public class AVLTree extends TwoForkLookupTree {
 
     }
 
+    public  void init(int[] numbers){
+        if (numbers==null||numbers.length==0){
+            return;
+        }else {
+            root=new Node(numbers[0],null,null,0,0);
+        }
+        for (int i=1;i<numbers.length;i++){
+            add(numbers[i]);
+        }
+    }
 
-    @Override
+
     public void  add(int number){
         if (root==null){
-            root=new Node(number,null,null,0);
+            root=new Node(number,null,null,0,0);
             return;
         }
         Node target=root;
         int height=0;
+        int right=0;
         while (target!=null){
+            height++;
             if (number==target.val){
                 break;
             }
             if (number>target.val){
+                right++;
                 if (target.rightChild==null){
-                    target.rightChild=new Node(number,null,null,height);
-                    checkHeight(target.rightChild,1);
+                    target.rightChild=new Node(number,null,null,height,right);
+//                    checkHeight(target.rightChild,1);
                     break;
                 }else {
                     if (number>target.val){
@@ -50,9 +69,10 @@ public class AVLTree extends TwoForkLookupTree {
                     }
                 }
             }else {
+                right--;
                 if (target.leftChild==null){
-                    target.leftChild=new Node(number,null,null,height);
-                    checkHeight(target.rightChild,2);
+                    target.leftChild=new Node(number,null,null,height,right);
+//                    checkHeight(target.rightChild,2);
                     break;
                 }else {
                     if (number>target.val){
@@ -81,14 +101,78 @@ public class AVLTree extends TwoForkLookupTree {
         }else {
             leftHeight=node.height;
         }
+    }
 
+
+
+
+
+    public ArrayDeque<Node> levelOrderTraversal() {
+        if (root == null) {
+            System.out.println("empty tree");
+            return null;
+        }
+        ArrayDeque<Node> queue = new ArrayDeque<>();
+        ArrayDeque<Node> result = new ArrayDeque<>();
+        queue.add(root);
+        while (queue.isEmpty() == false) {
+            Node node = queue.remove();
+            result.push(node);
+            if (node.leftChild != null) {
+                queue.add(node.leftChild);
+            }
+            if (node.rightChild != null) {
+                queue.add(node.rightChild);
+            }
+        }
+        return result;
+    }
+
+
+    @Override
+    public String toString() {
+        ArrayDeque<Node> elements=levelOrderTraversal();
+        if (elements==null){
+            return null;
+        }
+        int height=elements.getFirst().height;
+        String[] lines=new String[height+1];
+        int[] rightCounts=new int[height+1];
+//        StringBuilder sb=new StringBuilder();
+        while (!elements.isEmpty()){
+            Node temp=elements.pollLast();
+            if (lines[temp.height]==null){
+                lines[temp.height]=getInterval(height+temp.right)+ temp.val;
+                rightCounts[temp.height]=temp.right;
+            }
+            else {
+                lines[temp.height]=lines[temp.height]+getInterval(temp.right-rightCounts[temp.height])+ temp.val;
+            }
+        }
+        String result="";
+        for (String string:lines){
+            result+=string+"\r\n";
+        }
+        return result;
+    }
+
+    String getInterval(int number){
+        if (number<=0){
+            return "";
+        }else {
+            StringBuilder sb=new StringBuilder();
+            for (int i=1;i<=number;i++){
+                sb.append("     ");
+            }
+            return sb.toString();
+        }
     }
 
 
     public static void main(String[] args) {
         AVLTree avlTree=new AVLTree();
         avlTree.init(new int[]{1,7,6,8,12,55,9});
-        avlTree.lterator();
+        System.out.println(avlTree);
     }
 
 }
